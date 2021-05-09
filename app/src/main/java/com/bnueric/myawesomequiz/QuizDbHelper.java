b93f4bbf9809bed2e88dc5820bf04e59dd5340b0
EricM95 <60743062+EricM95@users.noreply.github.com>
2021-05-09 12:16:39 +0100

    Added difficulty levels

--- app/src/main/java/com/bnueric/myawesomequiz/QuizDbHelper.java
+++ app/src/main/java/com/bnueric/myawesomequiz/QuizDbHelper.java
@@ -33,7 +33,8 @@ public class QuizDbHelper extends SQLiteOpenHelper {
                 QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
                 QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
-                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER, " +
+                QuestionsTable.COLUMN_DIFFICULTY + " TEXT" +
                 ")";
 
         db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
@@ -50,20 +51,29 @@ public class QuizDbHelper extends SQLiteOpenHelper {
      * Seeds the database with dummy data
      */
     private void fillQuestionsTable(){
-        Question q1 = new Question("What is the largest country in the world?", "Russia", "China", "USA", 1);
+        Question q1 = new Question("Easy: What is the largest country in the world?",
+                "Russia", "China", "USA", 1, Question.DIFFICULTY_EASY);
         addQuestion(q1);
 
-        Question q2 = new Question("What year did World War 1 begin?", "1905", "1914", "1919", 2);
+        Question q2 = new Question("Easy: How many rings are on the Olympic flag?",
+                "4", "5", "7", 2, Question.DIFFICULTY_EASY);
         addQuestion(q2);
 
-        Question q3 = new Question("In what year did the Wall Street Crash take place", "1925", "1930", "1929", 3);
+        Question q3 = new Question("Medium: What is the 'trolley problem' an experiment in?",
+                "Physics", "Maths", "Philosophy", 3, Question.DIFFICULTY_MEDIUM);
         addQuestion(q3);
 
-        Question q4 = new Question("What is the scientific name of the kneecap", "Patella", "Femur", "Scapula", 1);
+        Question q4 = new Question("Medium: Which animal does not appear in the Chinese Zodiac?",
+                "Hummingbird", "Rabbit", "Dragon", 1, Question.DIFFICULTY_MEDIUM);
         addQuestion(q4);
 
-        Question q5 = new Question("Which of these is not a province in China", "Fujian", "Yangtze", "Guangdong", 2);
+        Question q5 = new Question("Hard: Who wrote The Scarlett Letter?",
+                "Shakespeare", "Nathanial Hawthorne", "Stephen King", 2, Question.DIFFICULTY_HARD);
         addQuestion(q5);
+
+        Question q6 = new Question("Hard: In which state did the Salem Witch Trials take place?",
+                "Washington", "Virginia", "Massachusetts", 3, Question.DIFFICULTY_HARD);
+        addQuestion(q6);
     }
 
     /**
@@ -77,13 +87,14 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
         cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
         cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        cv.put(QuestionsTable.COLUMN_DIFFICULTY, question.getDifficulty());
         db.insert(QuestionsTable.TABLE_NAME, null, cv);
     }
 
     public ArrayList<Question> getAllQuestions() {
         ArrayList<Question> questionList = new ArrayList<>();
         db = getReadableDatabase();
-        Cursor c = db.rawQuery( "SELECT* FROM " + QuestionsTable.TABLE_NAME, null);
+        Cursor c = db.rawQuery( "SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
 
         if (c.moveToFirst()) {
             do {
@@ -93,6 +104,32 @@ public class QuizDbHelper extends SQLiteOpenHelper {
                 question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
                 question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
                 question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                question.setDifficulty(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_DIFFICULTY)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+
+    public ArrayList<Question> getQuestions(String difficulty) {
+        ArrayList<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+
+        String[] selectionArgs = new String[]{difficulty};
+        Cursor c = db.rawQuery( "SELECT * FROM " + QuestionsTable.TABLE_NAME +
+                " WHERE " + QuestionsTable.COLUMN_DIFFICULTY + " = ?", selectionArgs);
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                question.setDifficulty(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_DIFFICULTY)));
                 questionList.add(question);
             } while (c.moveToNext());
         }
